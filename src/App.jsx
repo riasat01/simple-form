332c94e49a8322d02b50faf0915665c96006cd66
unknown <riasat01@outlook.com>
2023-09-23 00:07:29 +0600

    reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import './App.css'
 import HookForm from './components/hook-form/HookForm'
 import RefForm from './components/ref-form/RefForm'
+import ReusableForm from './components/resuable-form/ReusableForm'
 import SimpleForm from './components/simple-form/SimpleForm'
 import StatefulForm from './components/stateful-form/StatefulForm'
 
 function App() {
+
+  const handleSignIn = data => {
+    // e.preventDefault();
+    console.log(`sign up ${data}`)
+    console.log(`sign up `, data)
+  }
+  const handleUpdate = data => {
+    // e.preventDefault();
+    console.log(`update ${data}`)
+    console.log(`update `, data)
+  }
 
   return (
     <>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ReusableForm
+        formtitle={`sign in`}
+        handleSubmit={handleSignIn}
+      >
+        <section>
+          <h2>Sign in</h2>
+        </section>
+
+      </ReusableForm>
+      <ReusableForm
+      formtitle={`uodate profile`}
+      submitBtnText={`Update`}
+      handleSubmit={handleUpdate}>
+        <section>
+          <h2>Update</h2>
+        </section>
+      </ReusableForm>
     </>
   )
 }
---
+++ src/components/resuable-form/ReusableForm.jsx
@@ -0,0 +1,28 @@
+const ReusableForm = ({formtitle, submitBtnText = `Submit`, handleSubmit, children}) => { //children is default prop given by react
+
+    const handleSubmits = e => {
+        e.preventDefault();
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            phone: e.target.phone.value
+        }
+        handleSubmit(data);
+    }
+    return (
+        <div>
+            {children}
+            <form onSubmit={handleSubmits}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" />
+                <br />
+                <input type="number" name="phone" />
+                <br />
+                <button>{submitBtnText}</button>
+            </form>
+        </div>
+    );
+};
+
+export default ReusableForm;
